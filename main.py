9e5f9be0bc2dd67cad429d7c72a62f3bf65155be
Samuel <rcdsamu@gmail.com>
2022-05-08 21:16:06 +0200

    Main WiP

--- main.py
+++ main.py
@@ -46,11 +46,11 @@ def verify_password(plain_password, hashed_password):
 def get_password_hash(password):
     return pwd_context.hash(password)
 
-def authenticate_user(username: str, password: str, db: Session = Depends(get_db)):
-    user = crud.get_user_by_email(db, username)
+def authenticate_user(email: str, password: str, db: Session = Depends(get_db)):
+    user:models.Usuario = crud.get_user_by_email(db, email)
     if not user:
         return False
-    if not verify_password(password, user.hashed_password):
+    if not verify_password(password, user.contraseña):
         return False
     return user
 
@@ -72,28 +72,28 @@ async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = De
     )
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
+        email: str = payload.get("sub")
+        if email is None:
             raise credentials_exception
-        token_data = schemas.TokenData(username = username)
+        token_data = schemas.TokenData(email = email)
     except JWTError:
         raise credentials_exception
-    user = crud.get_user_by_email(db, token_data.username)
+    user = crud.get_user_by_email(db, token_data.email)
     if user is None:
         raise credentials_exception
     return user
 
 @app.post("/token", response_model=schemas.Token, responses={**responses.UNAUTORIZED}, tags=["auth"])
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = authenticate_user(form_data.username, form_data.password, db)
+    user = authenticate_user(form_data.email, form_data.password, db)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
+            detail="Incorrect email or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
+        data={"sub": user.email}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
