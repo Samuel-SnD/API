dd29c8e230eef95db54a723bb85184379de64860
Samuel <rcdsamu@gmail.com>
2022-05-11 18:16:55 +0200

    Main WiP

--- main.py
+++ main.py
@@ -120,7 +120,7 @@ async def get_user_by_email(user_email:str, db:Session = Depends(get_db), curren
 async def get_users(skip : int = 0, limit : int = 100 , db:Session = Depends(get_db), current_user:schemas.Usuario = Depends(get_current_user)) :
     return crud.get_users(db, skip, limit)
 
-@app.post("/users/", response_model = schemas.Usuario, responses = {**responses.USER_ALREADY_REGISTERED}, tags=["users"])
+@app.post("/users", response_model = schemas.Usuario, responses = {**responses.USER_ALREADY_REGISTERED}, tags=["users"])
 async def create_user(user:schemas.UsuarioCreate, db:Session = Depends(get_db)) :
     db_user = crud.get_user_by_email(db, user.correo)
     if db_user:
